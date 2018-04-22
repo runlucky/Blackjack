a9fb9b51f29d162469d57856d5928c345d9b5016
runlucky <stand.by.we@gmail.com>
2018-04-22 12:28:55 +0900

    ガワ作成

---
+++ Blackjack/Cards/Deck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blackjack
+{
+    public class Deck
+    {
+        private List<Card> cards;
+
+        
+    }
+}
---
+++ Blackjack/Cards/IDeck.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blackjack
+{
+    internal interface IDeck
+    {
+        bool IsEmpty();
+        bool HasCard();
+        Card Draw();
+        void Restore();
+    }
+}
